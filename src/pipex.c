f073d1ce73b30eb40f67cf1506186b36524d505b
Goh Yong Siang Ivan <gyong-si@c2r6s8.42singapore.sg>
2023-11-15 18:22:29 +0800

    added more code for pipex

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gyong-si <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:25:50 by gyong-si          #+#    #+#             */
-/*   Updated: 2023/11/10 17:50:30 by gyong-si         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:22:10 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int	main(int ac, char **av, char **envp)
 {
 	int	fd[2];
+	int	infile_fd;
+	int	outfile_fd;
+	char	*infile;
+	char	*outfile;
+	char	*cmd1;
+	char	*cmd2;
 
 	if (ac < 5)
 	{
+		infile = av[1];
+		cmd1 = av[2];
+		cmd2 = av[3];
+		outfile = av[4];
+	
 		if (pipe(fd) == -1)
 			return (1);
 		int pid1 = fork();
@@ -32,10 +44,15 @@ int	main(int ac, char **av, char **envp)
 		}
 		if (pid1 == 0)
 		{
+			infile_fd = open(infile, 0_RDONLY);
+			dup2(infile_fd, STDIN_FILENO);
+			close(infile_fd);
+
 			dup2(fd[1], STDOUT_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-			execlp("ping", "ping", "-c", "5", "google.com", NULL);
+
+			execlp("sh", "sh", "-c", cmd1, NULL);
 		}
 		int pid2 = fork();
 		if (pid2 < 0)
@@ -45,10 +62,15 @@ int	main(int ac, char **av, char **envp)
 		}
 		if (pid2 == 0)
 		{
-			dup2(fd[0], STDINT_FILENO);
+			dup2(fd[0], STDIN_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-			execlp("grep", "grep", "rtt", NULL);
+			
+			outfile_fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			dup2(outfile_fd, STDOUT_FILENO);
+			close(outfile_fd);
+
+			execlp("sh", "sh", "-c",cmd2, NULL);
 		}
 		close(fd[0]);
 		close(fd[1]);
@@ -57,13 +79,3 @@ int	main(int ac, char **av, char **envp)
 	}
 	return (0);
 }
-
-/**
-ft_init_pipex();
-ft_check_args();
-ft_parse_cmd();
-ft_parse_args()
-while (cmd)
-	ft_exec();
-ft_cleanup();
-**/
